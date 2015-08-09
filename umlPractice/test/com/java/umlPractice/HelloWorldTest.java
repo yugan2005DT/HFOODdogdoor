797d3975c86ecd6d8ca46c6b4b30ab7a40be44b9
yugan2005DT <ross.gump.wamu@gmail.com>
2015-08-09 09:13:56 -0700

    Setting for Git

---
+++ umlPractice/test/com/java/umlPractice/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.java.umlPractice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
